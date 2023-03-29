70de4db9da16ae0992a9657927cb00164134b199
AustinSpinazze <aspinazze94@gmail.com>
2023-03-28 21:36:13 -0500

    Added test User model The idea of adding this initial User modal is to gain an understanding of how a model will working in the Codex framework. In it we have a user class that stores data on a user (name & age), a getter and setter for retrieving and updating the instance of a user, and an on method that registers an event handler with this object, so other parts of the app know when something changes, and trigger method which triggers an event to tell other parts of the app that something has changes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hi there');
+import { User } from './models/Users';
+
+const user = new User({ name: 'Steve', age: 30 });
+
+user.on('change', () => {
+	console.log('Change #1');
+});
+user.on('change', () => {
+	console.log('Change #2');
+});
+user.on('save', () => {
+	console.log('Save triggered');
+});
+
+user.trigger('change');
+user.trigger('save');
+user.trigger('blah');
